2e45f83480ea1973ece9fe3c05a564d14e45cecb
Jack Bonanno <jackabonanno@gmail.com>
2025-12-07 22:20:56 -0500

    added some hyprlock fingerprint stuff, added some waybar vpn stuff

---
+++ .config/waybar/scripts/wg-status.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CONN="wg0"
+
+if ! nmcli connection show "$CONN" > /dev/null 2>%1; then
+    exit 0
+fi
+
+if nmcli -t -f DEVICE,STATE connection show --active | grep -q "^wg0:activated$"; then
+    echo "WG ↑"
+else
+    echo "WG ↓"
+fi
+
