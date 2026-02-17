22e0daf22842106ae5a54ab11aca2131ac20e32d
Jack Bonanno <jackabonanno@gmail.com>
2026-02-17 00:04:29 -0500

    changed temp for nightmode

--- .config/hypr/scripts/nightmode.sh
+++ .config/hypr/scripts/nightmode.sh
@@ -3,7 +3,7 @@
 CURRENT_TEMP=$(busctl --user get-property rs.wl-gammarelay / rs.wl.gammarelay Temperature | awk '{print $2}')
 
 if [ "$CURRENT_TEMP" -ge 6500 ]; then
-    busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 3500
+    busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 2500
 else
     busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500
 fi
